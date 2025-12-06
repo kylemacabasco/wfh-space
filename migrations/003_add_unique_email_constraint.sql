2938501f8a3de6de14d6c90a6a290c84d578e59e
kylemacabasco <kylemacabasco@gmail.com>
2025-12-05 22:37:37 -0800

    chore: removed a comment

--- migrations/003_add_unique_email_constraint.sql
+++ migrations/003_add_unique_email_constraint.sql
@@ -1,6 +1,5 @@
 -- Migration: Add unique constraint on email
 -- Run this in your Supabase SQL Editor (Dashboard > SQL Editor > New Query)
--- NOTE: Delete duplicate emails first before running this!
 
 -- Add unique constraint on email
 ALTER TABLE users ADD CONSTRAINT users_email_unique UNIQUE (email);
